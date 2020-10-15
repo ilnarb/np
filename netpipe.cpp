03b9613b4f3aeb75af39a5dc9f17e76f15819ff3
Ilnar Borkhanov <borhanov@corp.mail.ru>
2020-10-15 21:25:59 +0300

    first commit

---
+++ netpipe.cpp
@@ -0,0 +1,190 @@
+#include <stdarg.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int connect_inet(const char *host, int port, int timeout);
+
+int main(int argc, char *argv[])
+{
+	int ch;
+	int listen_port = -1;
+
+	while((ch = getopt(argc, argv, "l:")) >= 0)
+	{
+		switch(ch)
+		{
+		case 'l':
+			listen_port = atoi(optarg);
+			break;
+		}
+	}
+
+	if (listen_port > 0)
+	{
+		if (argc - optind > 0)
+		{
+			server_main(listen_port, argc - optind, argv + optind);
+			return 0;
+		}
+		return EXIT_FAILURE;
+	}
+
+	if (argv < 3)
+		return EXIT_FAILURE;
+
+	const char *host = argv[1];
+	int port = atoi(argv[2]);
+
+	// client
+	int fd = connect_inet(host, port, 5000);
+	if (fd)
+	{
+		std::thread thout([]{
+			while(true)
+			{
+				char buf[8*1024];
+				int n = read(fd, buf, sizeof(buf));
+				if (n < 0 && errno == EINTR) continue;
+				if (n <= 0) break;
+				fprintf(stdout, buf, n);
+			}
+		});
+		//
+		while(!feof(stdin))
+		{
+			char buf[8*1024];
+			int n1 = read(fileno(stdin), buf, sizeof(buf));
+			if (n1 < 0 && errno == EINTR) continue;
+			if (n1 <= 0) break;
+			
+			bool err = false;
+			int wr = 0;
+			do
+			{
+				int n2 = write(fd, buf + wr, n1 - wr);
+				if (n2 < 0 && errno == EINTR) continue;
+				if (n2 <= 0) { err = true; break; }
+				wr += n2;
+			}
+			while (wr < n1);
+			if (err) break;
+		}
+		//
+		shutdown(fd, SHUT_WR);
+		thout.join();
+		close(fd);
+	}
+
+	return 0;
+}
+
+int connect_inet(const char *host, int port, int timeout)
+{
+	struct addrinfo hints;
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC; // IPv4 or IPv6
+	hints.ai_socktype = SOCK_STREAM; // Stream socket type
+	hints.ai_protocol = IPPROTO_TCP; // TCP proto
+
+	char service[32];
+	sprintf(service, "%d", port);
+
+	struct addrinfo *addrinfo = NULL;
+	int err = getaddrinfo(host, service, &hints, &addrinfo);
+	if (err != 0)
+	{
+		fprintf(stderr, "Cannot resolve address %s:%s: %d (%s)\n", host, service, err, gai_strerror(err));
+		return false;
+	}
+
+	struct call_freeaddrinfo_t {
+		struct addrinfo *&_addrinfo;
+		call_freeaddrinfo_t(struct addrinfo *&addrinfo): _addrinfo(addrinfo) {}
+		~call_freeaddrinfo_t() { if (_addrinfo) { freeaddrinfo(_addrinfo); _addrinfo = NULL; } }
+	}
+	call_freeaddrinfo(addrinfo);
+
+	int fd = -1;
+	for(auto p = addrinfo; p != NULL; p = p->ai_next)
+	{
+		fd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)
+		if (fd == -1)
+		{
+			// Protocol is not supported on the host
+			fprintf(stderr, "failed to create socket: %d (%s)\n", errno, strerror(errno));
+			continue;
+		}
+
+		// Enable non-blocking I/O
+		int flags = fcntl(_fd, F_GETFL, 0);
+		if (flags == -1)
+		{
+			fprintf(stderr, "switching to async mode failed: %d (%s)\n", errno, strerror(errno));
+			close(fd); fd = -1;
+			continue;
+		}
+		//set nonblocing mode to use timeout while connecting
+		if (fcntl(_fd, F_SETFL, flags | O_NONBLOCK) == -1)
+		{
+			fprintf(stderr, "switching to async mode failed: %d (%s)\n", errno, strerror(errno));
+			close(fd); fd = -1;
+			continue;
+		}
+
+		// pending connect request
+		if (connect(_fd, p->ai_addr, p->ai_addrlen) < 0
+			&& errno != EINPROGRESS && errno != EINTR)
+		{
+			fprintf(stderr, "connect error: %d (%s)\n", errno, strerror(errno));
+			close(fd); fd = -1;
+			continue;
+		}
+
+		struct pollfd fds;
+		fds.fd = _fd;
+		fds.revents = 0;
+		fds.events = POLLOUT | POLLWRNORM;
+		int ret = poll(&fds, 1, timeout);
+		if (ret == 0)
+		{
+			fprintf(stderr, "connect timeout expired\n");
+			close(fd); fd = -1;
+			continue;
+		}
+		if (ret < 0)
+		{
+			fprintf(stderr, "connect error: %d (%s)\n", errno, strerror(errno));
+			close(fd); fd = -1;
+			continue;
+		}
+		//
+		int err = 0;
+		socklen_t len = sizeof(err);
+		if (getsockopt(_fd, SOL_SOCKET, SO_ERROR, &err, &len) < 0)
+		{
+			fprintf(stderr, "getsockopt failed: %d (%s)\n", errno, strerror(errno));
+			close(fd); fd = -1;
+			continue;
+		}
+		if (err)
+		{
+			fprintf(stderr, "cant connect: %d (%s)\n", err, strerror(err));
+			close(fd); fd = -1;
+			continue;
+		}
+
+		// return to blocking mode
+		if (fcntl(_fd, F_SETFL, flags) < 0)
+		{
+			fprintf(stderr, "cant switch to blocking mode: %d (%s)\n", err, strerror(err));
+			close(fd); fd = -1;
+			continue;
+		}
+
+		break;
+	}
+
+	return fd;
+}
---
+++ netpipe_server.cpp
@@ -0,0 +1,181 @@
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <memory>
+#include <thread>
+
+
+void sigchild(int num, siginfo_t *si, void *)
+{
+	if (!(si->si_code == CLD_STOPPED || si->si_code == CLD_CONTINUED))
+	{
+		int status = 0;
+		if (wait(&status) == -1) return;
+		if (WIFEXITED(status))
+			exit(WEXITSTATUS(status));
+		else
+			exit(1);
+	}
+}
+std::atomic_bool stop = false;
+void stop_signal(int num)
+{
+	stop = true;
+}
+
+typedef void (*sa_sigaction_t) (int, siginfo_t *, void *);
+sa_sigaction_t set_signal_3(int signum, sa_sigaction_t handler)
+{
+	struct sigaction act, oldact;
+
+	act.sa_sigaction = handler;
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = SA_SIGINFO;
+	act.sa_flags |= (signum == SIGALRM) ? SA_INTERRUPT : SA_RESTART;
+
+	if (sigaction(signum, &act, &oldact) < 0) return (sa_sigaction_t)-1;
+	return oldact.sa_sigaction;
+}
+
+void close_pipe(int *pipe)
+{
+	close(pipe[0]);
+	close(pipe[1]);
+}
+void close_on_exec(int fd)
+{
+	int flags = fcntl(fd, F_GETFD, 0);
+	if (fcntl(fd, F_SETFD, flags | FD_CLOEXEC) == -1)
+		fprintf(stderr, "process_t: fcntl(%d, F_SETFD, CLOEXEC) error: %d %s\n", fd, errno, strerror(errno));
+}
+
+void worker(int server_fd, int fd, const char *cmd)
+{
+	int out[2];
+	if (pipe(out) == -1)
+	{
+		fprintf(stderr, "pipe failed: %d (%s)\n", errno, strerror(errno));
+		return;
+	}
+	//
+	pid_t pid = fork();
+	if (pid == -1)
+	{
+		fprintf(stderr, "fork failed: %d (%s)\n", errno, strerror(errno));
+		close_pipe(out);
+		return;
+	}
+	if (pid == 0)
+	{
+		signal(SIGCHLD, SIG_DFL);
+		signal(SIGINT, SIG_DFL);
+		signal(SIGQUIT, SIG_DFL);
+		signal(SIGTERM, SIG_DFL);
+		signal(SIGPIPE, SIG_DFL);
+		//
+		close(server_fd);
+		close(out[1]);
+		//
+		if (dup2(fd, STDIN_FILENO) == -1)
+			fprintf(stderr, "process_t: dup2(%d, STDIN_FILENO) error: %d %s\n", fd, errno, strerror(errno));
+		if (dup2(out[1], STDOUT_FILENO) == -1)
+			fprintf(stderr, "process_t: dup2(%d, STDOUT_FILENO) error: %d %s\n", out[1], errno, strerror(errno));
+		close_pipe(out);
+		//
+		if (execl("/bin/sh", "sh", "-c", cmd, NULL) == -1)
+			fprintf(stderr, "process_t: execl(\"/bin/sh\", \"sh\", \"-c\", \"%s\", NULL) error: %d %s\n", cmd, errno, strerror(errno));
+		// should never happen
+		exit(0);
+	}
+
+	close(out[1]);
+	close_on_exec(out[0]);
+	
+	char buf[8*1024];
+	while(!stop.load())
+	{
+		int n1 = read(out[0], buf, sizeof(buf));
+		if (n1 < 0 && errno == EINTR) continue;
+		if (n1 <= 0) break;
+
+		bool err = false;
+		int wr = 0;
+		do
+		{
+			int n2 = write(fd, buf + wr, n1 - wr);
+			if (n2 < 0 && errno == EINTR) continue;
+			if (n2 <= 0) { err = true; break; }
+			wr += n2;
+		}
+		while (wr < n1);
+		if (err) break;
+	}
+
+	close(out[0]);
+	close(fd);
+}
+
+int server_main(int listen_port, int argc, char *argv[])
+{
+	int server_fd;
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+	{
+		return 1;
+	}
+	int onoff = 1;
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &onoff, sizeof(onoff)) < 0)
+	{
+		fprintf(stderr, "Cannot setsockopt(%d, SOL_SOCKET, SO_REUSEADDR, %d)\n", server_fd, onoff);
+	}
+
+	struct sockaddr_in sa;
+	memset(&sa, 0, sizeof(sa));
+	sa.sin_family = AF_INET;
+	sa.sin_addr = INADDR_ANY;
+	sa.sin_port = htons(listen_port);
+	//
+	if (bind(server_fd, (struct sockaddr*) &sa, sizeof(sa)) < 0)
+	{
+		fprintf(stderr, "Cannot bind\n");
+		close(server_fd);
+		return 1;
+	}
+	if (listen(server_fd, config.backlog) < 0)
+	{
+		fprintf(stderr, "Cannot listen %d\n", server_fd);
+		close(server_fd);
+		return 1;
+	}
+
+	set_signal_3(SIGCHLD, sigchild);
+	signal(SIGINT, stop_signal);
+	signal(SIGQUIT, stop_signal);
+	signal(SIGTERM, stop_signal);
+	signal(SIGPIPE, SIG_IGN);
+
+	std::vector<std::thread> threads;
+	while(!stop.load())
+	{
+		struct sockaddr_in sa;
+		socklen_t sa_len = sizeof(sa);
+
+		int fd = accept(server_fd, (struct sockaddr *) &sa, &sa_len);
+		if (fd >= 0)
+		{
+			threads.emplace_back(worker, server_fd, fd);
+		}
+	}
+
+	for(auto th : threads)
+		th.join();
+
+	return 0;
+}
