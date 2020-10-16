d708056a875bb1691bb0bdda617f6f916dbc624b
Ilnar Borkhanov <borhanov@corp.mail.ru>
2020-10-16 15:10:08 +0300

    handle socket and in/out failures

--- netpipe.cpp
+++ netpipe.cpp
@@ -11,7 +11,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+
 #include <thread>
+#include <atomic>
 
 int connect_inet(const char *host, int port, int timeout);
 int server_main(int listen_port, const char *cmd);
