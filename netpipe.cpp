372b792355091043e2211704182d258678ec7484
Ilnar Borkhanov <borhanov@corp.mail.ru>
2020-10-16 20:31:53 +0300

    fixes

--- netpipe.cpp
+++ netpipe.cpp
@@ -23,7 +23,8 @@ int status = 0;
 void sigurg(int)
 {
 	int n = recv(sock, &status, sizeof(status), MSG_OOB);
-	if (n != sizeof(status)) status = 0;
+	if (n != sizeof(status))
+		status = 0;
 	signal(SIGURG, sigurg);
 }
 
@@ -32,9 +33,9 @@ int main(int argc, char *argv[])
 	int ch;
 	int listen_port = -1;
 
-	while((ch = getopt(argc, argv, "l:c:")) >= 0)
+	while ((ch = getopt(argc, argv, "l:")) >= 0)
 	{
-		switch(ch)
+		switch (ch)
 		{
 		case 'l':
 			listen_port = atoi(optarg);
@@ -47,7 +48,7 @@ int main(int argc, char *argv[])
 		if (argc - optind > 0)
 		{
 			std::string cmd = argv[optind];
-			for(int i = optind + 1; i < argc; i++)
+			for (int i = optind + 1; i < argc; i++)
 			{
 				cmd += " ";
 				cmd += argv[i];
@@ -66,82 +67,82 @@ int main(int argc, char *argv[])
 
 	// client
 	sock = connect_inet(host, port, 5000);
-	if (sock >= 0)
-	{
-		std::atomic_bool failed = false;
-		// want to recv exit status using OOB data
-		fcntl(sock, F_SETOWN, getpid());
-		signal(SIGURG, sigurg);
-		//
-		std::thread thout([&]
+	if (sock < 0)
+		return EXIT_FAILURE;
+
+	std::atomic_bool failed = false;
+	// want to recv exit status using OOB data
+	fcntl(sock, F_SETOWN, getpid());
+	signal(SIGURG, sigurg);
+	//
+	std::thread thout([&] {
+		while (!failed.load())
 		{
-			while(!failed.load())
-			{
-				char buf[8*1024];
-				int n = recv(sock, buf, sizeof(buf), 0);
-				if (n < 0)
-				{
-					if (errno == EINTR)
-						continue;
-					failed = true;
-					break;
-				}
-				if (n > 0)
-					fwrite(buf, 1, n, stdout);
-				else
-					break;
-			}
-		});
-		//
-		while(!feof(stdin) && !failed.load())
-		{
-			char buf[8*1024];
-			int n1 = read(fileno(stdin), buf, sizeof(buf));
-			if (n1 < 0)
+			char buf[8 * 1024];
+			int n = recv(sock, buf, sizeof(buf), 0);
+			if (n < 0)
 			{
 				if (errno == EINTR)
 					continue;
 				failed = true;
 				break;
 			}
-			if (n1 == 0) break;
-
-			int wr = 0;
-			do
-			{
-				int n2 = send(sock, buf + wr, n1 - wr, 0);
-				if (n2 < 0)
-				{
-					if (errno == EINTR)
-						continue;
-					failed = true;
-					break;
-				}
-				if (n2 > 0)
-					wr += n2;
-				else
-					break;
-			}
-			while (wr < n1);
+			if (n > 0)
+				fwrite(buf, 1, n, stdout);
+			else
+				break;
 		}
-		//
-		shutdown(sock, SHUT_WR);
-		thout.join();
-		//
-		close(sock);
-		//
-		if (failed || !WIFEXITED(status))
-			return EXIT_FAILURE;
-	}
+	});
+	//
+	while (!feof(stdin) && !failed.load())
+	{
+		char buf[8 * 1024];
+		int n1 = read(fileno(stdin), buf, sizeof(buf));
+		if (n1 < 0)
+		{
+			if (errno == EINTR)
+				continue;
+			failed = true;
+			break;
+		}
+		if (n1 == 0)
+			break;
 
-	return EXIT_FAILURE;
+		int wr = 0;
+		do
+		{
+			int n2 = send(sock, buf + wr, n1 - wr, 0);
+			if (n2 < 0)
+			{
+				if (errno == EINTR)
+					continue;
+				failed = true;
+				break;
+			}
+			if (n2 > 0)
+				wr += n2;
+			else
+				break;
+		}
+		while (wr < n1);
+	}
+	//
+	shutdown(sock, SHUT_WR);
+	thout.join();
+	//
+	close(sock);
+	//
+	if (failed || !WIFEXITED(status))
+		return EXIT_FAILURE;
+
+	return 0;
 }
 
 int connect_inet(const char *host, int port, int timeout)
 {
 	struct addrinfo hints;
 	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = AF_UNSPEC; // IPv4 or IPv6
+	hints.ai_family = AF_UNSPEC;	 // IPv4 or IPv6
 	hints.ai_socktype = SOCK_STREAM; // Stream socket type
 	hints.ai_protocol = IPPROTO_TCP; // TCP proto
 
@@ -156,15 +157,22 @@ int connect_inet(const char *host, int port, int timeout)
 		return -1;
 	}
 
-	struct call_freeaddrinfo_t {
+	struct call_freeaddrinfo_t
+	{
 		struct addrinfo *&_addrinfo;
-		call_freeaddrinfo_t(struct addrinfo *&addrinfo): _addrinfo(addrinfo) {}
-		~call_freeaddrinfo_t() { if (_addrinfo) { freeaddrinfo(_addrinfo); _addrinfo = NULL; } }
-	}
-	call_freeaddrinfo(addrinfo);
+		call_freeaddrinfo_t(struct addrinfo *&addrinfo) : _addrinfo(addrinfo) {}
+		~call_freeaddrinfo_t()
+		{
+			if (_addrinfo)
+			{
+				freeaddrinfo(_addrinfo);
+				_addrinfo = NULL;
+			}
+		}
+	} call_freeaddrinfo(addrinfo);
 
 	int fd = -1;
-	for(auto p = addrinfo; p != NULL; p = p->ai_next)
+	for (auto p = addrinfo; p != NULL; p = p->ai_next)
 	{
 		fd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
 		if (fd == -1)
@@ -179,23 +187,25 @@ int connect_inet(const char *host, int port, int timeout)
 		if (flags == -1)
 		{
 			fprintf(stderr, "switching to async mode failed: %d (%s)\n", errno, strerror(errno));
-			close(fd); fd = -1;
+			close(fd);
+			fd = -1;
 			continue;
 		}
 		//set nonblocing mode to use timeout while connecting
 		if (fcntl(fd, F_SETFL, flags | O_NONBLOCK) == -1)
 		{
 			fprintf(stderr, "switching to async mode failed: %d (%s)\n", errno, strerror(errno));
-			close(fd); fd = -1;
+			close(fd);
+			fd = -1;
 			continue;
 		}
 
 		// pending connect request
-		if (connect(fd, p->ai_addr, p->ai_addrlen) < 0
-			&& errno != EINPROGRESS && errno != EINTR)
+		if (connect(fd, p->ai_addr, p->ai_addrlen) < 0 && errno != EINPROGRESS && errno != EINTR)
 		{
 			fprintf(stderr, "connect error: %d (%s)\n", errno, strerror(errno));
-			close(fd); fd = -1;
+			close(fd);
+			fd = -1;
 			continue;
 		}
 
@@ -207,13 +217,15 @@ int connect_inet(const char *host, int port, int timeout)
 		if (ret == 0)
 		{
 			fprintf(stderr, "connect timeout expired\n");
-			close(fd); fd = -1;
+			close(fd);
+			fd = -1;
 			continue;
 		}
 		if (ret < 0)
 		{
 			fprintf(stderr, "connect error: %d (%s)\n", errno, strerror(errno));
-			close(fd); fd = -1;
+			close(fd);
+			fd = -1;
 			continue;
 		}
 		//
@@ -222,13 +234,15 @@ int connect_inet(const char *host, int port, int timeout)
 		if (getsockopt(fd, SOL_SOCKET, SO_ERROR, &err, &len) < 0)
 		{
 			fprintf(stderr, "getsockopt failed: %d (%s)\n", errno, strerror(errno));
-			close(fd); fd = -1;
+			close(fd);
+			fd = -1;
 			continue;
 		}
 		if (err)
 		{
 			fprintf(stderr, "cant connect: %d (%s)\n", err, strerror(err));
-			close(fd); fd = -1;
+			close(fd);
+			fd = -1;
 			continue;
 		}
 
@@ -236,7 +250,8 @@ int connect_inet(const char *host, int port, int timeout)
 		if (fcntl(fd, F_SETFL, flags) < 0)
 		{
 			fprintf(stderr, "cant switch to blocking mode: %d (%s)\n", err, strerror(err));
-			close(fd); fd = -1;
+			close(fd);
+			fd = -1;
 			continue;
 		}
 
